1d576444c86e6dfae2299d3f8ef1ec9ec65e896b
Akash Prajapati <akash.prajapati@techabbot.com>
2024-05-03 19:45:08 +0530

    feat: started on api based login

---
+++ app/Models/CentralUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class CentralUser extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'password',
+        'company_name',
+        'erp_user_id',
+        'erp_user_id',
+        'erp_apiurl',
+        'erp_apiusername',
+        'erp_apipassword',
+    ];
+}
