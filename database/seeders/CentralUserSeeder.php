f5c69cd2bda67258bf4d7b9e96d9b86e4b98ff6f
Akash Prajapati <akash.prajapati@techabbot.com>
2024-06-21 13:19:57 +0530

    feat: updated the password

--- database/seeders/CentralUserSeeder.php
+++ database/seeders/CentralUserSeeder.php
@@ -16,7 +16,7 @@ class CentralUserSeeder extends Seeder
     {
         CentralUser::create([
             'username' => 'Admin',
-            'password' => Hash::make('Admin@123'),
+            'password' => Hash::make('Linx_123#@!'),
             'company_id' => '5730aaa7-fd77-e46f-298d-e8eca042d6a9',
             'erp_user_id' => 1,
             'erp_apiurl' => 'https://linxsystems.flowgear.net/dims24/',
